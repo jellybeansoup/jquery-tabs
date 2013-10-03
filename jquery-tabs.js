1099495af4640ee27dd28b5ac716e7c342046607
Daniel Farrelly <daniel@jellystyle.com>
2013-10-03 17:54:15 +1000

    Fixed a missing semicolon.

--- jquery-tabs.js
+++ jquery-tabs.js
@@ -86,7 +86,7 @@
 			var remove = $('<a></a>').addClass('ui-tab-remove').attr('href','#').html('Remove').appendTo( tab );
 			var pane = $('<div/>').addClass('ui-tab-content').attr('data-pane',id).html(content).appendTo( this._dom.container );
 			// Set the tab's text
-			link.html( title || pane.find('h1,h2,h3,h4,h5').first().addClass('ui-tab-title').text() )
+			link.html( title || pane.find('h1,h2,h3,h4,h5').first().addClass('ui-tab-title').text() );
 			// If this is the first tab, we're going to select it
 			if( this._dom.tabs === null || this._dom.tabs.length <= 0 ) {
 				tab.add(pane).addClass('selected');
